fbf21f17153e40218b3155012f7d41b82c81da7a
Boinzemwinde Josias Yannick Kologo <bjykologo@sqli.com>
2019-07-15 10:31:47 +0100

    admin.py added

---
+++ blog/admin.py
@@ -0,0 +1,47 @@
+from django.contrib import admin
+from .models import Content, Article, Offer, Picture, AboutMe, Category
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'is_visible', 'image')
+    list_filter = ('created_at', 'is_visible')
+    list_editable = ('is_visible', 'image')
+
+
+admin.site.register(Article, ArticleAdmin)
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'is_visible', 'image')
+    list_filter = ('created_at', 'is_visible')
+    list_editable = ('is_visible', 'image')
+
+
+admin.site.register(Offer, OfferAdmin)
+
+
+class PictureAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'is_visible', 'image', 'category')
+    list_filter = ('created_at', 'is_visible', 'category')
+    list_editable = ('is_visible', 'image')
+
+
+admin.site.register(Picture, PictureAdmin)
+
+
+class AboutMeAdmin(admin.ModelAdmin):
+    list_display = ('firstName', 'lastName', 'phoneNumber', 'email', 'image')
+    list_editable = ('phoneNumber', 'email', 'image')
+
+
+admin.site.register(AboutMe, AboutMeAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Content)
+
