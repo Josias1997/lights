72483ab3c0e963a1cd7a7b481169ff2ac6066908
josias <kologojosias@gmail.com>
2019-09-04 04:06:17 +0100

    settings updated

--- myblog/settings.py
+++ myblog/settings.py
@@ -33,6 +33,9 @@ EMAIL_HOST_USER = 'kologojosias@gmail.com'
 EMAIL_HOST_PASSWORD = 'kingjosias'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+# EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'kologojosias@gmail.com')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'kingjosias')
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000
 # Application definition
