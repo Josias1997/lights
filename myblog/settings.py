3cf837f2c3b3abcf4071a3ef2ee051c1e3ca5a05
josias <kologojosias@gmail.com>
2019-09-04 03:40:47 +0100

    static files

--- myblog/settings.py
+++ myblog/settings.py
@@ -95,10 +95,10 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dLDMUMU0xU',
-        'USER': 'dLDMUMU0xU',
-        'PASSWORD': 'B0LYd1zqfO',
-        'HOST': 'remotemysql.com',
+        'NAME': 'godson$lightsphotography',
+        'USER': 'godson',
+        'PASSWORD': 'Kngyjb199*#',
+        'HOST': 'godson.mysql.pythonanywhere-services.com',
         'PORT': '3306'
     }
 }
